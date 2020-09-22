1d8bd85bd0f73dc5a96245d4d4485483ab7833cc
FreshyMichael <michael@freshysites.com>
2020-09-22 08:59:42 -0400

    Update foreTees_passThrough.php

--- foreTees_passThrough.php
+++ foreTees_passThrough.php
@@ -13,10 +13,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-/* PluginName Start */
+/* ForeTees Member Pass-Through Start */
 //______________________________________________________________________________
 
 
+function fortees_passThru_function(){
+
+		$current_user = wp_get_current_user();
+		$user_name= esc_html( $current_user->user_login );
+		/*Golf Login Form*/
+		echo "<form action='https://www1.foretees.com/v5/servlet/Login' method='POST' target='_blank'><div class='hiddenPassThrough' style='display:none;'><input type='text' id='clubname' name='clubname' value='binghamtoncc'><input type='text' id='user_name' name='user_name' value='$user_name'><input type='text' id='activity' name='activity' value='0'><input type='text' id='caller' name='caller' value='PDG4735'></div><input type='submit' class='ft_lgn_bttn' value='ForeTees Login'></form>";
+			
+	}
+
+add_shortcode('fortees_passThru', 'fortees_passThru_function');
+/* Plugin Styles */
+function foreTees_styles() {
+    wp_enqueue_style('foreTees_styles', plugin_dir_url(__FILE__) . 'includes/foretees_styles.css', false, '1.0.0', 'all');
+}
+add_action( 'wp_enqueue_scripts', 'foreTees_styles' );
 //______________________________________________________________________________
 // All About Updates
 
@@ -42,5 +57,5 @@ $myUpdateChecker->getVcsApi()->enableReleaseAssets();
 //$myUpdateChecker->setBranch('stable-branch-name');
 
 //______________________________________________________________________________
-/* PluginName End */
+/* ForeTees Member Pass-Through End */
 ?>
